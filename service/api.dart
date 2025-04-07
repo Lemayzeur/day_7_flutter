c67e521639a7edb8239fd0a935c4867fe9353594
Lub Lorry Lamysère <lemayzeur@code9haiti.com>
2025-04-07 18:13:32 -0500

    Create api.dart

---
+++ service/api.dart
@@ -0,0 +1,20 @@
+Map<String, dynamic> data = {
+  'users': [
+    {
+      "id": 1,
+      "first_name": "Jean",
+      "last_name": "Robert",
+      "email": "jean.robert@esih.edu",
+      "password": "@1234",
+      "photo": "https://png.pngtree.com/png-clipart/20231019/original/pngtree-user-profile-avatar-png-image_13369988.png"
+    },
+    {
+      "id": 2,
+      "first_name": "ADM",
+      "last_name": "Narcisse",
+      "email": "adm.narcisse@esih.edu",
+      "password": "@0000",
+      "photo": "https://png.pngtree.com/png-clipart/20231019/original/pngtree-user-profile-avatar-png-image_13369988.png"
+    }
+  ],
+};
